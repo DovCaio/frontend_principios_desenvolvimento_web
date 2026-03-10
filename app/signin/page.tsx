d248f6498a88e00393af56fd4279e3686d730673
DovCaio <onlydeadnotalive@gmail.com>
2026-03-10 20:43:52 -0300

    feat: faz a tela de login.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,25 +1,32 @@
+import { BiBuildings } from "react-icons/bi";
+
 export default function SignIn() {
   return (
     <div className="bg-blue-100 h-screen flex items-center justify-center">
-        <div className="flex flex-col h-fit items-center justify-center bg-white rounded-lg shadow-lg p-8 m-4">
-            <h2>LOGO</h2>
-            <h1 className="text-2xl font-bold">Condominio Manager</h1>
-            <span>Entre com seu CPF e senha</span>
+        <div className="flex flex-col h-fit bg-white rounded-lg shadow-lg p-8 m-4 w-1/3">
+            <div className="flex flex-col items-center mb-4 w-full">
+                <div className="bg-blue-600 rounded-full p-2 mb-4">
+                    <BiBuildings className="text-4xl text-white bg-blue-600 rounded-full" />
+
+                </div>
+                <h1 className="text-2xl text-color-gray-900 font-bold ">Condomínio Manager</h1>
+                <span className="text-gray-500 font-semibold">Entre com seu CPF e senha</span>
+            </div>
 
             <div>
 
 
-                <div className="flex flex-col">
-                    <label htmlFor="cpf">CPF</label>
-                    <input type="text" id="cpf" className="bg-gray-200 rounded-lg h-8" name="cpf" />
+                <div className="flex flex-col my-4">
+                    <label htmlFor="cpf" className="font-semibold">CPF</label>
+                    <input type="text" id="cpf" className="bg-gray-200 rounded-lg h-8 p-2" name="cpf" />
 
                 </div>
-                <div>
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" id="password" name="password" />
+                <div className="flex flex-col my-4">
+                    <label htmlFor="password" className="font-semibold">Senha</label>
+                    <input type="password" id="password" className="bg-gray-200 rounded-lg h-8 p-2" name="password" />
                 </div>
-                <div>
-                    <button type="submit">Entrar</button>
+                <div className="my-4">
+                    <button type="submit" className="rounded-lg bg-gray-950 text-white w-full h-8 hover:cursor-pointer">Entrar</button>
                 </div>
             </div>
 
